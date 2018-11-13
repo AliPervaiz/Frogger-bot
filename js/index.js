2cd9d82b7bc337386248defe8e7d60645dee50c0
Alex DeJong <alexdejong737@gmail.com>
2018-11-13 15:11:18 -0600

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -7,5 +7,33 @@ function draw() {
 	var c = color(153, 153, 153);
 	fill(c);
 	noStroke();
-  rect(0, 500, window.innerWidth, 100);
+	rect(0, 500, window.innerWidth, 100);
+}
+
+/////////////
+// TERRAIN //
+/////////////
+
+class Terrain
+{
+	constructor(color, movingObject)
+	{
+		this.color = color;
+		this.movingObject = movingObject;
+		this.children = [];
+	}
+	
+	draw(width, cellHeight){
+		noStroke();
+		fill(this.color);
+		rect(-width/2, 0, width, 1);
+		
+		if(this.movingObject){
+			
+		}
+	}
+	
+	removeChild(){
+		children.shift();
+	}
 }
