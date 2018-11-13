a64885a8cb3678306316b1b1f5412f987797072b
Blake <bo134593@g.risd.org>
2018-11-13 16:06:08 -0600

    manages the generation of moving objects

--- js/index.js
+++ js/index.js
@@ -16,24 +16,50 @@ function draw() {
 
 class Terrain
 {
-	constructor(color, movingObject)
+	constructor(color, movingObjectGenerator)
 	{
 		this.color = color;
-		this.movingObject = movingObject;
-		this.children = [];
+		this.movingObjectGenerator = movingObjectGenerator;
+		this.cooldown = 0;
+
+		//generate all initial moving objects
+		if(movingObjectGenerator){
+			this.children = [];
+			let nextObject;
+			do{
+				nextObject = movingObjectGenerator();
+				this.children.push(nextObject);
+			}while(nextObject.cooldown==0);
+			this.cooldown = nextObject.cooldown;
+		}
 	}
 	
 	draw(width, cellHeight){
 		noStroke();
 		fill(this.color);
-		rect(-width/2, 0, width, 1);
+		rect(-width/2, -.5, width, .5);
 		
-		if(this.movingObject){
-			
+		if(this.movingObjectGenerator){
+			//wait for the cooldown to reach 0. once it does, add a new moving object to children
+			if(--this.cooldown <= 0){
+				let nextObject = this.movingObjectGenerator();
+				this.cooldown = nextObject.cooldown;
+				this.children.push(nextObject);
+			}
+
+			//draw each child
+			for(let i=this.children.length-1;i>-1;i--)
+				children[i].draw();
 		}
+
 	}
 	
 	removeChild(){
 		children.shift();
 	}
 }
+
+/////////////////////
+// M.O. GENERATORS //
+/////////////////////
+
